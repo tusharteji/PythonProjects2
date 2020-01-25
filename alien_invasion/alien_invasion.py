ed88b426a20c1bbaf366565009e4c1e3311723c5
tusharteji <tushar.teji@gmail.com>
2020-01-25 14:19:55 -0600

    Added left movement of the ship

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -27,9 +27,14 @@ class AlienInvasion:
                 if event.key == pygame.K_RIGHT:
                     # Move the ship to the right
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    # Move the ship to the left
+                    self.ship.moving_left = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
