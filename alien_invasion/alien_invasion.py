b184e9ff0864e01dbf3d03a8f7ebf0692e378d0f
tusharteji <tushar.teji@gmail.com>
2020-01-25 16:20:14 -0600

    Refactoring _check_events() method

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -24,17 +24,28 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    # Move the ship to the right
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    # Move the ship to the left
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+   
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            # Turn on the movement flag for right movement.
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            # Turn on the movement flag for left movement.
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            # Turn off the movement flag to stop right movement.
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            # Turn off the movement flag to stop left movement.
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
