bb92063b028df7c16fb1e3b5b96072c6c7162f68
tusharteji <tushar.teji@gmail.com>
2020-01-25 12:22:15 -0600

    Adding right movement of ship

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -23,6 +23,13 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
@@ -37,6 +44,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
 
